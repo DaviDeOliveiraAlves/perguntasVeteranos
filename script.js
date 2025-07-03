00d9f923d09ee37b8688543dbbdbfc64ac1bd689
DaviDeOliveiraAlves <davi.oa.work@gmail.com>
2025-07-03 15:54:15 -0300

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ function mostrarPergunta(tipo) {
     if (tipo === "soft") {
         pergunta = "Como você lidaria com um prazo curto de entrega?";
     } else if (tipo === "hard") {
-        pergunta = "Qual sinal define algo como string em um código?";
+        pergunta = "Qual operador é usado para comparar igualdade em JavaScript?";
     }
 
     document.getElementById("pergunta").textContent ="🕵️" + pergunta;
@@ -11,6 +11,6 @@ function mostrarPergunta(tipo) {
     const resposta = prompt(pergunta);
 
     if (resposta) {
-        document.getElementById("resposta").textContent = "Sua resposta: " + resposta;
+        document.getElementById("resposta").textContent = "🕺Sua resposta: " + resposta;
     }
 }
